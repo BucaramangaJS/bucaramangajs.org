f132f11316e54ebf60144d9a22f4b93d820f6c6e
Romel Pérez <ronelprhone@gmail.com>
2017-05-11 17:09:52 -0500

    [update] website

--- client/index.js
+++ client/index.js
@@ -1,3 +1,14 @@
 $(function () {
+
   $(".button-collapse").sideNav();
+
+  $('.side-nav a').on('click', () => {
+    $('.button-collapse').sideNav('hide');
+  });
+
+  $('.menu-list a').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+  });
 });
